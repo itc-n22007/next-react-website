dd288d4b59c44c9c77e4bc5364ed79a9e9ea9529
itc-n22007 <n22007@std.it-college.ac.jp>
2023-01-12 07:44:50 +0000

    P168まで

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import Container from 'components/container'
 import Hero from '@/components/hero'
 import PostBody from 'components/post-body'
+import Image from 'next/image'
+import eyecatch from 'images/about.jpg'
 import Contact from 'components/contact'
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
 
@@ -11,6 +13,16 @@ export default function About() {
      title='About'
      subtitle='About development activities'
      />
+     <figure>
+      <Image
+        src={eyecatch}
+        alt=""
+        layout="responsive"
+        sizes="(min-width: 1152px) 1152px, 100vw"
+        priority
+        placeholder='blur'
+       />
+     </figure>
         <TwoColumn>
 	 <TwoColumnMain>
 	  <PostBody>
