c3e6c0ff77c78c2c809430a2c8fb1151b86e2292
itc-n22007 <n22007@std.it-college.ac.jp>
2023-01-25 07:57:56 +0000

    1/25

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from '@/components/hero'
 import PostBody from 'components/post-body'
@@ -8,7 +9,14 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-colum
 
 export default function About() {
   return (
-    <Container>  
+    <Container> 
+	 <Meta 
+	  pageTitle="アバウト"
+	  pageDesc="About development activities"
+	  pageImg={eyecatch.src}
+	  pageImgW={eyecatch.width}
+	  pageImgH={eyecatch.height}
+	  />
      <Hero
      title='About'
      subtitle='About development activities'
